593e1201408e76149dcd570b78338163bc0c576e
clearsky77 <clearsky3264@gmail.com>
2021-09-12 19:49:59 +0900

    방의 상세 정보 화면 UI 작업

---
+++ app/src/main/java/com/clearsky77/jickbangcopy_20210912/ViewRoomDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.clearsky77.jickbangcopy_20210912
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewRoomDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_room_detail)
+    }
+}
