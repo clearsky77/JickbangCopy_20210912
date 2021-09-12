a4840f898b266223c0bb29fc026a4901f7607e43
clearsky77 <clearsky3264@gmail.com>
2021-09-12 13:42:47 +0900

    설명 문구까지 화면에 반영되도록함.

--- app/src/main/java/com/clearsky77/jickbangcopy_20210912/adapters/RoomAdapter.kt
+++ app/src/main/java/com/clearsky77/jickbangcopy_20210912/adapters/RoomAdapter.kt
@@ -5,9 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.clearsky77.jickbangcopy_20210912.R
 import com.clearsky77.jickbangcopy_20210912.datas.RoomData
-
 class RoomAdapter(
     val mContext: Context, //어느 화면에
     val resId: Int, //
@@ -24,6 +24,14 @@ class RoomAdapter(
         val row = tempRow!! // null이 아니다는 뜻.
 //        getView 작업의 결과물로 지정.
         val data = mList[position]
+
+        val priceTxt = row.findViewById<TextView>(R.id.priceTxt)
+        val addressAndFloorTxt = row.findViewById<TextView>(R.id.addressAndFloorTxt)
+        val descriptionTxt = row.findViewById<TextView>(R.id.descriptionTxt)
+
+        descriptionTxt.text = data.description
+
         return row
     }
 }
+
