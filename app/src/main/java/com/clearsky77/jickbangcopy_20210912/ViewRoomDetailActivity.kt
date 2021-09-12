0b09f5f787cca4005d7f5f38ba5d64bea453d5f0
clearsky77 <clearsky3264@gmail.com>
2021-09-12 20:15:50 +0900

    main.xml에 list에 방 클릭시 -> 방의 디테일 화면으로 넘어감.

--- app/src/main/java/com/clearsky77/jickbangcopy_20210912/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/clearsky77/jickbangcopy_20210912/ViewRoomDetailActivity.kt
@@ -3,6 +3,7 @@ package com.clearsky77.jickbangcopy_20210912
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.clearsky77.jickbangcopy_20210912.datas.RoomData
+import kotlinx.android.synthetic.main.activity_view_room_detail.*
 
 class ViewRoomDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +13,9 @@ class ViewRoomDetailActivity : AppCompatActivity() {
         val room = intent.getSerializableExtra("roomData") as RoomData
         //roomData라는 이름의 짐을 받는데, RoomData의 형태로 받는다.
 
-
-
+        priceTxt.text = room.getFormattedPrice()
+        descriptionTxt.text = room.description
+        addressTxt.text = room.address
+        floorTxt.text = room.getFormattedFloor()
     }
 }
