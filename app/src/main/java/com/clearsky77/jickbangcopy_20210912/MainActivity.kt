483743c1a938c71d43f0e9f05ee2c2fecd771a28
clearsky77 <clearsky3264@gmail.com>
2021-09-12 13:38:02 +0900

    Adapter을 만들어서 리스트 뷰와 연결

--- app/src/main/java/com/clearsky77/jickbangcopy_20210912/MainActivity.kt
+++ app/src/main/java/com/clearsky77/jickbangcopy_20210912/MainActivity.kt
@@ -2,10 +2,34 @@ package com.clearsky77.jickbangcopy_20210912
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.clearsky77.jickbangcopy_20210912.adapters.RoomAdapter
+import com.clearsky77.jickbangcopy_20210912.datas.RoomData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val mRoomList = ArrayList<RoomData>()
+    lateinit var mRoomAdapter: RoomAdapter //lateinit은 나중에 쓴다는 뜻.
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //임시방편: 코드에 타이핑해서 추가
+
+        mRoomList.add(RoomData(9000, "서울시 동대문구", 5, "1번째 방입니다."))
+        mRoomList.add(RoomData(6000, "서울시 동대문구", 5, "1번째 방입니다."))
+        mRoomList.add(RoomData(9000, "서울시 동대문구", 0, "1번째 방입니다."))
+        mRoomList.add(RoomData(8000, "서울시 동대문구", 5, "1번째 방입니다."))
+        mRoomList.add(RoomData(9000, "서울시 동대문구", -1, "1번째 방입니다."))
+        mRoomList.add(RoomData(10000, "서울시 동대문구", 1, "1번째 방입니다."))
+        mRoomList.add(RoomData(12000, "서울시 동대문구", 3, "1번째 방입니다."))
+        mRoomList.add(RoomData(15000, "서울시 동대문구", 10, "1번째 방입니다."))
+
+        mRoomAdapter = RoomAdapter(this, R.layout.room_list_item, mRoomList)
+        roomListView.adapter = mRoomAdapter
+
     }
 }
---
+++ app/src/main/java/com/clearsky77/jickbangcopy_20210912/adapters/RoomAdapter.kt
@@ -0,0 +1,29 @@
+package com.clearsky77.jickbangcopy_20210912.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.clearsky77.jickbangcopy_20210912.R
+import com.clearsky77.jickbangcopy_20210912.datas.RoomData
+
+class RoomAdapter(
+    val mContext: Context, //어느 화면에
+    val resId: Int, //
+    val mList: ArrayList<RoomData>) : ArrayAdapter<RoomData>(mContext, resId, mList) {
+
+    val mInflater = LayoutInflater.from(mContext)
+
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView //임시로 한 줄에 해당하는 것을 만들어 줌.
+        if (tempRow == null) {
+            tempRow = mInflater.inflate(R.layout.room_list_item, null) // R이라는 것은 res 폴더다!
+        }
+        val row = tempRow!! // null이 아니다는 뜻.
+//        getView 작업의 결과물로 지정.
+        val data = mList[position]
+        return row
+    }
+}
