48159d493c5824e70cb81c02c920eebe1e47c11f
clearsky77 <clearsky3264@gmail.com>
2021-09-22 21:54:32 +0900

    모든 주석 삭제 - 주석 연습용

--- app/src/main/java/com/clearsky77/jickbangcopy_20210912/adapters/RoomAdapter.kt
+++ app/src/main/java/com/clearsky77/jickbangcopy_20210912/adapters/RoomAdapter.kt
@@ -15,12 +15,12 @@ class RoomAdapter(  val mContext: Context,
     val mInflater = LayoutInflater.from(mContext)
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        var tempRow = convertView //임시로 한 줄에 해당하는 것을 만들어 줌.
+        var tempRow = convertView
         if (tempRow == null) {
-            tempRow = mInflater.inflate(R.layout.room_list_item, null) // R이라는 것은 res 폴더다!
+            tempRow = mInflater.inflate(R.layout.room_list_item, null)
         }
-        val row = tempRow!! // null이 아니다는 뜻.
-        val data = mList[position] // getView 작업의 결과물로 지정.
+        val row = tempRow!!
+        val data = mList[position]
 
         val priceTxt = row.findViewById<TextView>(R.id.priceTxt)
         val addressAndFloorTxt = row.findViewById<TextView>(R.id.addressAndFloorTxt)
